e0b57b1d46e524140a3c75282f05ef1b6a9c088d
robertoruedaq <rueda.roberto89@gmail.com>
2017-10-17 23:05:08 -0500

    search system

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,13 @@
 class Search < ApplicationRecord
+	def search_schools
+		schools = School.all
+ 		schools = shools.where(["state LIKE ?", state]) if state.present?
+ 		schools = shools.where(["city LIKE ?", city]) if city.present?
+ 		schools = shools.where(["gender LIKE ?", gender]) if gender.present?
+ 		schools = shools.where(["levels LIKE ?", levels]) if levels.present?
+ 		schools = shools.where(["languages LIKE ?", languages]) if languages.present?
+ 		schools = shools.where(["owner LIKE ?", owner]) if owner.present?
+ 		return schools
+ 	end
 end
+ 
