a42176edc7af2f13427f4a9023bb9e29a7794084
cmg <desarrollo@cmg.com.co>
2017-10-19 14:40:11 -0500

    organize the views

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,16 @@
 class School < ApplicationRecord
+	geocoded_by :full_address
+	after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+	
 	def self.search(search)
 	  where("school_name LIKE ?", "%#{search}%")
+	end
+
+	def full_address
+		[address, city, state].compact.join(', ')
+	end
+
+	def coords
+		"{lat:""#{latitude}, ""lng:""#{longitude}}"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :schools
   resources :searches
 
-  root to: "schools#index"
+  root to: "searches#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
